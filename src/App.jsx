98053669844dfdad2f59f8be07e23075611a4447
Ma-Eduarda <eduardabritto2014@gmail.com>
2025-06-19 15:01:11 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layouts/Layout.jsx";
 import Home from "./Pages/Home.jsx";
 import Instituicoes from "./Pages/Instituicoes.jsx";
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="Instituicoes" element={<Instituicoes />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
